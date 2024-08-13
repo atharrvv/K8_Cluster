de6825ee6827da7ffd555be76132829993783ef5
Atharv <105142090+atharrvv@users.noreply.github.com>
2024-08-13 14:34:36 +0530

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "pairss" {
+resource "aws_key_pair" "K8_pair" {
   key_name   = "K8_pair"
   public_key = file("~/.ssh/id_rsa.pub")  # Path to your existing public key file
 }
